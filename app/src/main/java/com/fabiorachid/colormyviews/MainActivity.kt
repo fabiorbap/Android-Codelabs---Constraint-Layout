744b429e6df5e136c17bf58849f6499065974498
Fabio Rachid <fabiorbap@gmail.com>
2020-11-25 20:42:18 -0300

    Finish implementing game

--- app/src/main/java/com/fabiorachid/colormyviews/MainActivity.kt
+++ app/src/main/java/com/fabiorachid/colormyviews/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +21,9 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.red_button -> box_three_text.setBackgroundColor(ContextCompat.getColor(this, R.color.my_red))
+            R.id.yellow_button -> box_four_text.setBackgroundColor(ContextCompat.getColor(this, R.color.my_yellow))
+            R.id.green_button -> box_five_text.setBackgroundColor(ContextCompat.getColor(this, R.color.my_green))
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
@@ -27,7 +31,7 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners() {
         val clickableViews: List<View> = listOf(
             box_one_text, box_two_text, box_three_text, box_four_text, box_five_text,
-            constraint_layout
+            constraint_layout, red_button, green_button, yellow_button
         )
         for (item in clickableViews) item.setOnClickListener { makeColored(it) }
     }
